bf96a747f7af34a31b7bbd78ef2b9ae161eaad3d
GreenCyborgNinja <148530350+GreenCyborgNinja@users.noreply.github.com>
2026-01-14 13:08:25 +0100

    Update next.config.ts

--- wai-frontend/next.config.ts
+++ wai-frontend/next.config.ts
@@ -1,8 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  // WICHTIG: Erzwingt eine statische HTML-Version
+  
+  // WICHTIG: Ersetze 'wai-frontend' mit deinem EXAKTEN Repo-Namen auf GitHub
+  basePath: '/wai-frontend', 
+  
+  images: {
+    unoptimized: true, // WICHTIG: Next.js Image-Optimierung geht nicht auf GitHub Pages
+  },
 };
 
 export default nextConfig;
